47d055cf15f44f76e139d46debd2bc316fcd9d2f
richardtam.ny <richardtam.ny@gmail.com>
2019-02-08 18:30:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(seconds)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds)
+  counter = seconds
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter = counter - 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
