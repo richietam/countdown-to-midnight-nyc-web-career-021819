2a3571a96bf6335b888e8a566f4d752ee49aad0b
richardtam.ny <richardtam.ny@gmail.com>
2019-02-08 18:24:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+  counter = seconds
+  while counter > 0
+    puts " #{counter} SECOND(S)!"
+    counter -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
